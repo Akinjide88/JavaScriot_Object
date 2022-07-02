f9ad63476fd7051d4d5a7612228bdbfe77a16d79
Akinjide <jideoscar88@gmail.com>
2022-07-02 13:07:56 +0100

    JavaScript Object

---
+++ array.js
@@ -0,0 +1,179 @@
+const worker1 = {
+    no: 1,
+    FirstName: "Thompson",
+    LastName: "Anthony",
+    Age: 32,
+    Sex: "Male",
+    Position: "Head Of Finance Department",
+    Date_of_Employment: "17/07/2014",
+}
+
+const worker2 = {
+    no: 2,
+    FirstName: "Johnson",
+    LastName: "Mary",
+    Age: 23,
+    Sex: "Female",
+    Position: "Sofware Engineer",
+    Date_of_Employment: "05/03/2019",
+}
+
+const worker3 = {
+    no: 3,
+    FirstName: "Chisom",
+    LastName: "Onyeka",
+    Age: 28,
+    Sex: "Female",
+    Position: "Human Resource Manager",
+    Date_of_Employment: "21/06/2016",
+}
+
+const worker4 = {
+    no: 4,
+    FirstName: "Jide",
+    LastName: "Olabisi",
+    Age: 29,
+    Sex: "Male",
+    Position: "Assistant Head (Software Department)",
+    Date_of_Employment: "05/06/2016",
+}
+
+const worker5 = {
+    no: 5,
+    FirstName: "Williams",
+    LastName: "Ajayi",
+    Age: 30,
+    Sex: "Male",
+    Position: "Head of Software Department",
+    Date_of_Employment: "06/09/2015",
+}
+
+const worker6 = {
+    no: 6,
+    FirstName: "Nancy",
+    LastName: "Chukwuemeka",
+    Age: 27,
+    Sex: "Female",
+    Position: "Secretary",
+    Date_of_Employment: "23/08/2016",
+}
+
+const worker7 = {
+    no: 7,
+    FirstName: "Nicholas",
+    LastName: "Uchemba",
+    Age: 26,
+    Sex: "Male",
+    Position: "Software Engineer",
+    Date_of_Employment: "22/02/2020",
+}
+
+const worker8 = {
+    no: 8,
+    FirstName: "Ahmed",
+    LastName: "Olayinka",
+    Age: 24,
+    Sex: "Male",
+    Position: "Graphic Designer",
+    Date_of_Employment: "15/08/2020",
+}
+
+const worker9 = {
+    no: 9,
+    FirstName: "Halima",
+    LastName: "Mohammed",
+    Age: 26,
+    Sex: "Female",
+    Position: "Senior Finance Manager",
+    Date_of_Employment: "30/05/2018",
+}
+
+const worker10 = {
+    no: 10,
+    FirstName: "Richard",
+    LastName: "Adeyemi",
+    Age: 41,
+    Sex: "Male",
+    Position: "C.E.O",
+    Date_of_Employment: "17/11/2010",
+}
+
+document.getElementById("w1_td1").innerHTML = worker1.no;
+document.getElementById("w1_td2").innerHTML = worker1.FirstName;
+document.getElementById("w1_td3").innerHTML = worker1.LastName;
+document.getElementById("w1_td4").innerHTML = worker1.Age;
+document.getElementById("w1_td5").innerHTML = worker1.Sex;
+document.getElementById("w1_td6").innerHTML = worker1.Position;
+document.getElementById("w1_td7").innerHTML = worker1.Date_of_Employment;
+
+document.getElementById("w2_td1").innerHTML = worker2.no;
+document.getElementById("w2_td2").innerHTML = worker2.FirstName;
+document.getElementById("w2_td3").innerHTML = worker2.LastName;
+document.getElementById("w2_td4").innerHTML = worker2.Age;
+document.getElementById("w2_td5").innerHTML = worker2.Sex;
+document.getElementById("w2_td6").innerHTML = worker2.Position;
+document.getElementById("w2_td7").innerHTML = worker2.Date_of_Employment;
+
+document.getElementById("w3_td1").innerHTML = worker3.no;
+document.getElementById("w3_td2").innerHTML = worker3.FirstName;
+document.getElementById("w3_td3").innerHTML = worker3.LastName;
+document.getElementById("w3_td4").innerHTML = worker3.Age;
+document.getElementById("w3_td5").innerHTML = worker3.Sex;
+document.getElementById("w3_td6").innerHTML = worker3.Position;
+document.getElementById("w3_td7").innerHTML = worker3.Date_of_Employment;
+
+document.getElementById("w4_td1").innerHTML = worker4.no;
+document.getElementById("w4_td2").innerHTML = worker4.FirstName;
+document.getElementById("w4_td3").innerHTML = worker4.LastName;
+document.getElementById("w4_td4").innerHTML = worker4.Age;
+document.getElementById("w4_td5").innerHTML = worker4.Sex;
+document.getElementById("w4_td6").innerHTML = worker4.Position;
+document.getElementById("w4_td7").innerHTML = worker4.Date_of_Employment;
+
+document.getElementById("w5_td1").innerHTML = worker5.no;
+document.getElementById("w5_td2").innerHTML = worker5.FirstName;
+document.getElementById("w5_td3").innerHTML = worker5.LastName;
+document.getElementById("w5_td4").innerHTML = worker5.Age;
+document.getElementById("w5_td5").innerHTML = worker5.Sex;
+document.getElementById("w5_td6").innerHTML = worker5.Position;
+document.getElementById("w5_td7").innerHTML = worker5.Date_of_Employment;
+
+document.getElementById("w6_td1").innerHTML = worker6.no;
+document.getElementById("w6_td2").innerHTML = worker6.FirstName;
+document.getElementById("w6_td3").innerHTML = worker6.LastName;
+document.getElementById("w6_td4").innerHTML = worker6.Age;
+document.getElementById("w6_td5").innerHTML = worker6.Sex;
+document.getElementById("w6_td6").innerHTML = worker6.Position;
+document.getElementById("w6_td7").innerHTML = worker6.Date_of_Employment;
+
+document.getElementById("w7_td1").innerHTML = worker7.no;
+document.getElementById("w7_td2").innerHTML = worker7.FirstName;
+document.getElementById("w7_td3").innerHTML = worker7.LastName;
+document.getElementById("w7_td4").innerHTML = worker7.Age;
+document.getElementById("w7_td5").innerHTML = worker7.Sex;
+document.getElementById("w7_td6").innerHTML = worker7.Position;
+document.getElementById("w7_td7").innerHTML = worker7.Date_of_Employment;
+
+document.getElementById("w8_td1").innerHTML = worker8.no;
+document.getElementById("w8_td2").innerHTML = worker8.FirstName;
+document.getElementById("w8_td3").innerHTML = worker8.LastName;
+document.getElementById("w8_td4").innerHTML = worker8.Age;
+document.getElementById("w8_td5").innerHTML = worker8.Sex;
+document.getElementById("w8_td6").innerHTML = worker8.Position;
+document.getElementById("w8_td7").innerHTML = worker8.Date_of_Employment;
+
+document.getElementById("w9_td1").innerHTML = worker9.no;
+document.getElementById("w9_td2").innerHTML = worker9.FirstName;
+document.getElementById("w9_td3").innerHTML = worker9.LastName;
+document.getElementById("w9_td4").innerHTML = worker9.Age;
+document.getElementById("w9_td5").innerHTML = worker9.Sex;
+document.getElementById("w9_td6").innerHTML = worker9.Position;
+document.getElementById("w9_td7").innerHTML = worker9.Date_of_Employment;
+
+document.getElementById("w10_td1").innerHTML = worker10.no;
+document.getElementById("w10_td2").innerHTML = worker10.FirstName;
+document.getElementById("w10_td3").innerHTML = worker10.LastName;
+document.getElementById("w10_td4").innerHTML = worker10.Age;
+document.getElementById("w10_td5").innerHTML = worker10.Sex;
+document.getElementById("w10_td6").innerHTML = worker10.Position;
+document.getElementById("w10_td7").innerHTML = worker10.Date_of_Employment;
